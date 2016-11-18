587ed507a96f0397122d0e04a1eec7598f585555
Keval <keval.multidots@gmail.com>
2016-11-18 14:42:24 +0530

    :-)

--- DigitalClockWatchface/wear/src/main/java/watchface/androidsample/com/digitalclockwatchface/DigitalClock.java
+++
@@ -1,7 +0,0 @@
-package watchface.androidsample.com.digitalclockwatchface;
-
-import android.support.wearable.watchface.CanvasWatchFaceService;
-
-public class DigitalClock extends CanvasWatchFaceService {
-
-}
