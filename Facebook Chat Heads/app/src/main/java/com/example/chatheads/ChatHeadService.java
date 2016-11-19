452a5a0e367c98c1c9c144b344691040bdb8a9f8
kevalpatel2106 <kevalpatel2106@gmail.com>
2016-11-19 15:15:13 +0530

    Facebook chat heads: Commenting and updating readme

--- Facebook Chat Heads/app/src/main/java/com/example/chatheads/ChatHeadService.java
+++ Facebook Chat Heads/app/src/main/java/com/example/chatheads/ChatHeadService.java
@@ -1,14 +1,9 @@
 package com.example.chatheads;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.PixelFormat;
-import android.graphics.Point;
 import android.os.IBinder;
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -32,17 +27,8 @@ public class ChatHeadService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        //Inflate the chat head layout we created
         mChatHeadView = LayoutInflater.from(this).inflate(R.layout.layout_chat_head, null);
-
-        //Set the close button.
-        ImageView closeButton = (ImageView) mChatHeadView.findViewById(R.id.close_btn);
-        closeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //close the service and remove the chat heads
-                stopSelf();
-            }
-        });
 
         //Add the view to the window.
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
@@ -51,17 +37,30 @@ public class ChatHeadService extends Service {
                 WindowManager.LayoutParams.TYPE_PHONE,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.TOP | Gravity.LEFT;
+
+        //Specify the chat head position
+        params.gravity = Gravity.TOP | Gravity.LEFT;        //Initially view will be added to top-left corner
         params.x = 0;
         params.y = 100;
 
+        //Add the view to the window
         mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         mWindowManager.addView(mChatHeadView, params);
 
-        //Set chat head image view.
+        //Set the close button.
+        ImageView closeButton = (ImageView) mChatHeadView.findViewById(R.id.close_btn);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //close the service and remove the chat head from the window
+                stopSelf();
+            }
+        });
+
+        //Drag and move chat head using user's touch action.
         final ImageView chatHeadImage = (ImageView) mChatHeadView.findViewById(R.id.chat_head_profile_iv);
         chatHeadImage.setOnTouchListener(new View.OnTouchListener() {
-            public int lastAction;
+            private int lastAction;
             private int initialX;
             private int initialY;
             private float initialTouchX;
@@ -71,14 +70,23 @@ public class ChatHeadService extends Service {
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+
+                        //remember the initial position.
                         initialX = params.x;
                         initialY = params.y;
+
+                        //get the touch location
                         initialTouchX = event.getRawX();
                         initialTouchY = event.getRawY();
+
                         lastAction = event.getAction();
                         return true;
                     case MotionEvent.ACTION_UP:
-                        if (lastAction == MotionEvent.ACTION_DOWN){
+                        //As we implemented on touch listener with ACTION_MOVE,
+                        //we have to check if the previous action was ACTION_DOWN
+                        //to identify if the user clicked the view or not.
+                        if (lastAction == MotionEvent.ACTION_DOWN) {
+                            //Open the chat conversation click.
                             Intent intent = new Intent(ChatHeadService.this, ChatActivity.class);
                             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent);
@@ -89,10 +97,11 @@ public class ChatHeadService extends Service {
                         lastAction = event.getAction();
                         return true;
                     case MotionEvent.ACTION_MOVE:
-                        params.x = initialX
-                                + (int) (event.getRawX() - initialTouchX);
-                        params.y = initialY
-                                + (int) (event.getRawY() - initialTouchY);
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+
+                        //Update the layout with new X & Y coordinate
                         mWindowManager.updateViewLayout(mChatHeadView, params);
                         lastAction = event.getAction();
                         return true;
@@ -106,19 +115,5 @@ public class ChatHeadService extends Service {
     public void onDestroy() {
         super.onDestroy();
         if (mChatHeadView != null) mWindowManager.removeView(mChatHeadView);
-    }
-
-    private int dpToPixel(int dp) {
-        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
-        float px = dp * (metrics.densityDpi / 160f);
-        return Math.round(px);
-    }
-
-    public static int displayWidth(Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return size.x;
     }
 }
